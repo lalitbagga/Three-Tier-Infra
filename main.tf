a5573cc217c1d681229d47850d0afc381b1ac709
Lalit Bagga <l.bagga@combyne.ag>
2026-05-11 16:08:43 -0400

    Remove unnecessary port settings for bastion security group egress rule in Terraform configuration

--- main.tf
+++ main.tf
@@ -148,8 +148,6 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_outbound" {
     security_group_id = aws_security_group.bastion_sg.id
-    from_port         = 0
-    to_port           = 0
     ip_protocol =  "-1"
     cidr_ipv4       = "0.0.0.0/0" 
 }
